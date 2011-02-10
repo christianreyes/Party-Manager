11addfe7291f4c2c724f31a9ccc8f616e487d0d5
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-02-10 00:03:33 -0500

    scaffolded users with authentication

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.3'
 gem 'sqlite3-ruby', :require => 'sqlite3'
-gem 'nifty-generators'
 gem 'will_paginate'
 
 # Bundle edge Rails instead:
@@ -27,7 +26,10 @@ gem 'will_paginate'
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
 # and rake tasks are available in development mode:
-# group :development, :test do
-#   gem 'webrat'
-# end
+group :development, :test do
+  #gem 'webrat'
+  gem "rails-erd"
+  gem 'nifty-generators'
+end
 gem "mocha", :group => :test
+gem "bcrypt-ruby", :require => "bcrypt"
---
+++ db/schema.rb
@@ -0,0 +1,71 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20110210045954) do
+
+  create_table "guest_invitations", :force => true do |t|
+    t.integer  "party_id"
+    t.integer  "guest_id"
+    t.integer  "invited_by"
+    t.string   "invite_code"
+    t.integer  "expected_attendees"
+    t.integer  "actual_attendees"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "locations", :force => true do |t|
+    t.string   "name"
+    t.string   "street"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "parties", :force => true do |t|
+    t.string   "name"
+    t.integer  "host_id"
+    t.text     "details"
+    t.integer  "party_type_id"
+    t.date     "date"
+    t.time     "start_time"
+    t.time     "end_time"
+    t.date     "rsvp_date"
+    t.integer  "location_id"
+    t.boolean  "public_party"
+    t.boolean  "public_guestlist"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "party_types", :force => true do |t|
+    t.string   "name"
+    t.boolean  "active"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
