f03e3b2a2781ff59bb7b2a6f3f73dd28d2b28c8d
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-02-27 17:48:57 -0500

    address method added

--- app/models/location.rb
+++ app/models/location.rb
@@ -6,4 +6,30 @@ class Location < ActiveRecord::Base
     has_many :users
     
     validates_presence_of :name, :latitude, :longitude
+    
+    def address
+    	str = String.new
+    	str = comma_add(str, street)
+    	str = comma_add(str, city)
+    	str = comma_add(str, state)
+    	if(zip)
+    		if(str.length > 0)
+    		str+= " "
+    		end
+    		str += zip
+    	end
+    	
+    	return str
+    end
+    
+    def comma_add(str, add)
+    	if(add)
+    		if(str.length>0)
+    			str += ', '
+        	end
+        	str += add
+        end
+        
+        return str
+    end
 end
