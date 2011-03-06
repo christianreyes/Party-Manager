ab5e10e8a6bd2ad81d18832b608105b72f31626b
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-03-06 14:15:01 -0800

    added validation

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -9,4 +9,8 @@ class Guest < ActiveRecord::Base
     
     scope :all, order(:name.asc)
     
+    validates_presence_of :name, :email
+    
+    validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+    
 end
