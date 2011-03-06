2d3ad8fa34f0cd6a2d93f6db7107e9e0660f743f
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-03-05 23:04:47 -0800

    fixed migration

--- db/migrate/20110306064047_add_host_id_to_location.rb
+++ db/migrate/20110306064047_add_host_id_to_location.rb
@@ -1,6 +1,6 @@
 class AddHostIdToLocation < ActiveRecord::Migration
   def self.up
-    add_column :locations, :host_id, :string
+    add_column :locations, :host_id, :integer
   end
 
   def self.down
