a03314a54ce7aae4fd030ea4632d9c05a78fa69b
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-03-08 15:08:53 -0800

    datepicker reverted back

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
 	def string_to_date(str)
-		return Date.strptime(str, '%m/%d/%Y') if str.match(/\d+\/\d+\/\d+/)
-		return Date.parse(str) if str.match(/[A-Z][a-z]+ \d+, \d+/)
-		return nil
+		if str
+			return Date.strptime(str, '%m/%d/%Y') if str.match(/\d+\/\d+\/\d+/)
+			return Date.parse(str) if str.match(/[A-Z][a-z]+ \d+, \d+/)
+			return nil
+		end
 	end
 end
