f6b746da98b7766d14283a8d8dcebb7c48cbfdf1
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-03-08 10:21:19 -0800

    invite code

--- app/models/guest_invitation.rb
+++ app/models/guest_invitation.rb
@@ -1,14 +1,22 @@
 class GuestInvitation < ActiveRecord::Base
     attr_accessible :id, :party_id, :guest_id, :invited_by, :invite_code, 
     				:expected_attendees, :actual_attendees
+    				
+    before_create :generate_invite_code
     
     belongs_to :party
-    belongs_to :guest, :dependent => :destroy
+    belongs_to :guest
     belongs_to :user, :class_name => "User", :foreign_key => "invited_by"
     
     has_many :gifts, :dependent => :destroy
     #has_one :guest, :dependent => :destroy
     
     validates_presence_of :party_id, :guest_id
+   
+   
+    private 
     
+    def generate_invite_code
+    	self.invite_code = rand(36**16).to_s(5)
+    end 
 end
