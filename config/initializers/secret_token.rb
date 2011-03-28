140ed2f8fd311d9ec1c61b6120733f3c50836892
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-03-28 16:07:51 -0400

    initial commit of new party manager app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyManger::Application.config.secret_token = '7c299daf7f155b75e4907c081c1c6a42b70921d912981b4dba2bfc15dfd42c3c5d0884840a90b9d31eced4b3ebd731006f0549d03212c73af23a38951e873f3f'
